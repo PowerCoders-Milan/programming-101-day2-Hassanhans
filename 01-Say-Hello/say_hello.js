fd286d5efc46c8ae060347630df480d5bcec0365
hassanwasswa <assanwasswa@gmail.com>
2021-02-19 12:21:18 +0100

    execrise 1

--- 01-Say-Hello/say_hello.js
+++ 01-Say-Hello/say_hello.js
@@ -1,8 +1,14 @@
 //insert your pseudocode below
 /*
-
-here
+Create the an input of your name first
+prompt for text with message of your name
+Create text with message and your name
+Print the mesaage
+Print
 
 */
 
 //insert your code below
+name2 = window.prompt('Your Name ');
+message = 'Hello' + String(name2);
+window.alert(message);
